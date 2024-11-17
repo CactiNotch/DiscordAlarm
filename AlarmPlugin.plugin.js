c3789b59fddb58becd8d98b1992da872a371edb5
CactiNotch <86714173+CactiNotch@users.noreply.github.com>
2024-11-18 10:08:01 +1100

    init

---
+++ AlarmPlugin.plugin.js
@@ -0,0 +1,11 @@
+/**
+ * @name AlarmPlugin
+ * @author James Golob
+ * @description Setup an alarm within your Discord chats and calls!
+ * @version 0.0.1
+ */
+
+module.exports = (meta) => ({
+  start() {},
+  stop() {},
+});
